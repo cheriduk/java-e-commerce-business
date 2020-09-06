9e004323a2fdfde90681e282afc7d42950f96dcb
AllenDu <15162678717@163.com>
2020-09-06 18:49:22 +0800

    Initial commit

---
+++ course-01-high-concurrency/src/main/java/com/cheri/course01highconcurrency/Course01HighConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.cheri.course01highconcurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course01HighConcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course01HighConcurrencyApplication.class, args);
+    }
+
+}
---
+++ course-01-high-concurrency/src/test/java/com/cheri/course01highconcurrency/Course01HighConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheri.course01highconcurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course01HighConcurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
